1696cd65208da4f5a9e116bc2f3a10f8b14290de
Krunalsinh Vaghela <krunalsinh.vaghela@corpnet.co.in>
2023-10-30 17:43:02 +0530

    datat comparision

--- 01_basics/02_dataTypes.js
+++ 01_basics/02_dataTypes.js
@@ -1,27 +1,49 @@
 "use strict"; // treat all js code as newer version
 
+/*actual datatypes*/
+// 1) 'number' 
+// 2) 'bigint' 
+// 3) 'string' 
+// 4) 'boolean' 
+// 5) 'object' 
+// 6) 'function' (function object)
+// 7) 'symbol' 
+// 8) 'undefined' 
+
+
+/* primitive data type */
 //number  = 1234;
-//bigint
+//BigInt
 //string  = "abcd";
 //boolean  = true/false;
 //undefined
 //null => stand alone value
 //symbol => unique
 
-//object
+/* non primitive ( reference ) data type */
+//Array, object, functions
 
 let a = ['1','2','3'];
 const b = 1150;
-const c = "abcd";
-const d = true;
+const c = 1150n;
+const d = "abcd";
+const e = true;
+const f = {id: "1123", name : "abc"};
+const g = (a) => a*a;
+const h = Symbol('123');
+
 
 console.table([{
-    a_array: typeof a, 
-    b_number : typeof b, 
-    c_string : typeof c, 
-    d_boolean: typeof d,
-    e_null: typeof null,
-    f_undefined: typeof undefined,
-    g_NaN : typeof NaN
+    a_array: typeof a, //  object
+    b_number : typeof b, // number
+    c_bigint : typeof c, // bigint
+    d_string : typeof d, // string
+    e_boolean: typeof e, // boolean
+    f_object: typeof f, // object
+    g_function: typeof g, // function
+    h_symbol: typeof h, // symbol
+    i_null: typeof null, // object
+    j_undefined: typeof undefined, // undefined
+    k_NaN : typeof NaN // number
 }])
 
--- 01_basics/05_comparision.js
+++ 01_basics/05_comparision.js
@@ -15,3 +15,13 @@
 
 // console.log(2 == "2"); // true (data type conversion "2" -> 2)
 // console.log(2 === "2"); // false (also check data type)
+
+// console.log(true == 1); // true (data type conversion,, true -> 1)
+// console.log(true === 1); // false (data type conversion,, true -> 1 but also type check which is not same)
+// console.log(true > 0); // true (data type conversion,, true -> 1)
+// console.log(false == 0); // true (data type conversion,, false -> 0)
+// console.log(false < 1); // true (data type conversion,, false -> 0)
+
+let a = Symbol('123');
+let b = Symbol('123');
+console.log(a === b); //false
