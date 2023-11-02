a89089b170322129fb3f8995e2c10581aba2ddc3
Krunalsinh <krunalsinh7718@gmail.com>
2023-11-02 10:03:24 +0530

    arrays

---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,8 @@
+//1) declare methods
+const arr1 = ['a', 'b', 'c']; // shortcut for new Array(). called an array literal/array shorthand.
+const arr2 = new Array('a', 'b', 'c');
+// console.log('arr1 =>',arr1,' arr2 =>', arr2, '. They are fundamentally similar.');
+
+const arr3 = new Array(3);
+console.log('arr3 =>',arr3 , 'arr3[0] =>', arr3[0], 'arr3.length =>',arr3.length," new Array(3) declares 3 undefined(empty) elements." );
+
