84d1c02b69e63c97f0fe4e3d546292bcfa2f4b5c
Krunalsinh Vaghela <krunalsinh.vaghela@corpnet.co.in>
2023-11-10 16:25:17 +0530

    loops

--- 05_loops/loops.js
+++ 05_loops/loops.js
@@ -40,10 +40,50 @@
 
 // 5) do while loop ############################################################
 
-let count2 = 15;
-do {
-    console.log(count2);
-    count2++;
-} while(count2 < 10)
+// let count2 = 15;
+// do {
+//     console.log(count2);
+//     count2++;
+// } while(count2 < 10)
 //in this loop we can print first value of count2 event if its not matching count
 //because we are matching condition after statement execution
+
+// 6) for of loop ############################################################
+// let arr1 = ['apple', 'banana', 'cherry', 'dragon fruit', 'elderberry'];
+
+// for (const i of arr1) {
+//     console.log(`fruit ${i}`);
+// }
+
+
+// const map = new Map();
+// map.set('name', "krunalsinh");
+// map.set('surname', "vaghela");
+// map.set('name', "vaghela");
+
+// for (const [key, value] of map) {
+//     console.log(`${key} => ${value}`);
+// }
+
+// 7) for in loop ############################################################
+const games = {
+    'game1' : 'NFS',
+    'game2' : 'call of duty',
+    'game3' : 'gta 5',
+    'game4' : 'forza horizan 5',
+}
+
+for (const game in games) {
+    console.log(`${game} => ${games[game]}`);
+}
+let arr2 = ['apple', 'banana', 'cat', 'dog', 'elephant'];
+
+for(const e in arr2){
+    console.log(` ${e} : ${arr2[e]}`);
+}
+
+//we cant use "for of" loop for object because object is not iterable. for of loop works on only iterable data.
+//we have to use "for in" loop. this loop can access object keys. we can iterate key one by one. 
+
+// 8) array.forEach ############################################################
+let arr3 = ['apple', 'banana', 'cat', 'dog', 'elephant'];
